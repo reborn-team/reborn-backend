3591b848be9605cd9660488b0e125027416b5f5b
drunkenhw <drunkenhw@gmail.com>
2022-08-30 14:27:27 +0900

    Fix Address 필드명 변경

--- src/main/java/com/reborn/reborn/entity/Address.java
+++ src/main/java/com/reborn/reborn/entity/Address.java
@@ -5,10 +5,10 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class Address {
 
-    private String city;
-
     //TODO 도로명 주소 변수명 변경해야함.
     private String roadName;
+
+    private String detailAddress;
 
     private String zipcode;
 }
