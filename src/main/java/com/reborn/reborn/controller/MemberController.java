918727e8dd96d7c1533bee8068226da943c95bc2
drunkenhw <drunkenhw@gmail.com>
2022-09-01 20:19:51 +0900

    Fix : 회원가입 구현

---
+++ src/main/java/com/reborn/reborn/controller/MemberController.java
@@ -0,0 +1,26 @@
+package com.reborn.reborn.controller;
+
+import com.reborn.reborn.dto.MemberRequestDto;
+import com.reborn.reborn.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/join")
+    public ResponseEntity join(@RequestBody MemberRequestDto memberRequestDto){
+        memberService.registerMember(memberRequestDto);
+        return ResponseEntity.ok().build();
+    }
+}
