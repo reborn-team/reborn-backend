4c9debc49f8319ef63ca19244cb6f864cb692e1f
drunkenhw <drunkenhw@gmail.com>
2022-09-03 21:06:53 +0900

    Fix : 개인 운동 정보 및 운동 부위별 카테고리 추가

--- src/test/java/com/reborn/reborn/controller/TestController.java
+++ src/test/java/com/reborn/reborn/controller/TestController.java
@@ -5,12 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@WebAppConfiguration
 @AutoConfigureMockMvc // -> webAppContextSetup(webApplicationContext)
 @AutoConfigureRestDocs // -> apply(documentationConfiguration(restDocumentation))
 @SpringBootTest
@@ -18,15 +27,25 @@ public class TestController {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private SecurityFilterChain filterChain;
 
 
     @Test
-    void getMemberTest() throws Exception {
-
-        mockMvc.perform(get("/member/{name}", "john"))
+    @WithAnonymousUser
+    public void 메인화면() throws Exception{
+        mockMvc.perform(get("/"))
                 .andDo(print())
-                .andDo(document("getMember")); //test 수행시 snippets 로 생성해줌 ( 파일명 )
-//                .andExpect(jsonPath("$.name", is("john")));
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
+
+//    @Test
+//    void postJoinTest(){
+//        mockMvc.perform(
+//                post("/api/v1/join")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//        ).andExpect(status())
+//    }
 }
