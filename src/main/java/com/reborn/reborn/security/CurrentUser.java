90180a30714536d031931aa264d4468cec92558e
drunkenhw <drunkenhw@gmail.com>
2022-09-05 00:42:12 +0900

    Feat : JWT 발급 프로세스 구현

---
+++ src/main/java/com/reborn/reborn/security/CurrentUser.java
@@ -0,0 +1,12 @@
+package com.reborn.reborn.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : member")
+public @interface CurrentUser {
+}
---
+++ src/main/java/com/reborn/reborn/security/jwt/TokenProvider.java
@@ -0,0 +1,28 @@
+package com.reborn.reborn.security.jwt;
+
+import com.reborn.reborn.entity.MemberRole;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Slf4j
+public class TokenProvider {
+
+    private final Key key;
+
+    public TokenProvider(String secret){
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public AuthToken createAuthToken(String id, MemberRole role , Date expireDate){
+        return new AuthToken(id, role, expireDate, key);
+    }
+
+    public AuthToken converterAuthToken(String token){
+        return new AuthToken(token, key);
+    }
+
+}
