5632a8a22c07f51fe8f0af768705e1ecd8713b4a
drunkenhw <drunkenhw@gmail.com>
2022-09-29 19:24:18 +0900

    Feat : Valid 조건 추가

--- src/main/java/com/reborn/reborn/dto/MemberRequestDto.java
+++ src/main/java/com/reborn/reborn/dto/MemberRequestDto.java
@@ -4,15 +4,21 @@ import com.reborn.reborn.entity.Address;
 import com.reborn.reborn.entity.Member;
 import com.reborn.reborn.entity.MemberRole;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberRequestDto {
-
+    @Email
     private String email;
     private String password;
+    @NotNull
+    @Length(min = 1, message = "수정할 이름의 길이는 1이상이어야 합니다.")
     private String nickname;
     private String phone;
     private String zipcode;
